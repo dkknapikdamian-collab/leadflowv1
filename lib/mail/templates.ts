f431972f15897c2d5518495bc02456c5dcafa342
dkknapikdamian-collab <dk.knapikdamian@gmail.com>
2026-04-07 15:51:31 +0200

    Update templates.ts

--- lib/mail/templates.ts
+++ lib/mail/templates.ts
@@ -14,11 +14,11 @@ export interface AccountStatusEmailTemplateInput {
 
 function escapeHtml(value: string) {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
 }
 
 export function formatPolishDate(value: string | null) {
