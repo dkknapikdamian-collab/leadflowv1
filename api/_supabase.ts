8cadb41ae905226cae0abc48b0bcdb0401e9bbcf
dkknapikdamian-collab <dk.knapikdamian@gmail.com>
2026-04-22 22:52:25 +0200

    Napraw restore workspace i scope API

--- api/_supabase.ts
+++ api/_supabase.ts
@@ -1,6 +1,6 @@
 type RecordMap = Record<string, unknown>;
 
-function isUuid(value: unknown) {
+export function isUuid(value: unknown) {
   return typeof value === 'string'
     && /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
 }
