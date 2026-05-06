f11b785c950af37a1510f095526ad6f246be786a
Damian <dk.knapikdamian@gmail.com>
2026-05-06 17:25:35 +0200

    chore: reduce hobby serverless function count to 12

--- api/workspace-subscription.ts
+++
@@ -1,6 +0,0 @@
-import systemHandler from './system.js';
-
-export default async function handler(req: any, res: any) {
-  req.query = { ...(req.query || {}), kind: 'workspace-subscription' };
-  return systemHandler(req, res);
-}
