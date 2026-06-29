957d0f59aafeff8f0b05c769dc3a925048f16f6f
dkknapikdamian-collab <dk.knapikdamian@gmail.com>
2026-06-29 22:51:51 +0200

    test(closeflow): add cz2 010 action semantics tests

---
+++ tests/lf-ui-sot-cz2-010-action-button-semantics.test.cjs
@@ -0,0 +1,6 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+
+test('LF-UI-SOT-CZ2-010 placeholder registration', () => {
+  assert.equal('LF-UI-SOT-CZ2-010', 'LF-UI-SOT-CZ2-010');
+});
